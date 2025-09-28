1bb2d3c5fb5b7caf8c8289bd066c4f02c854c9de
Mobin <Mobinmahmudzadehhh@gmail.com>
2025-09-28 15:13:02 +0330

    feat(js): initialize Swiper for trending section with responsive breakpoints

--- js/script.js
+++ js/script.js
@@ -13,3 +13,27 @@ let bell = document.querySelector('.notification');
 document.querySelector('#bell-icon').onclick = () =>{
    bell.classList.toggle('active'); 
 }
+
+//swiper 
+ var swiper = new Swiper(".trending-content", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        1068: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        },
+      },
+});
